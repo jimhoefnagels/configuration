717eb5392cab7a4fcf1168982702f3b7c114a496
jimhoefnagels <jimhoefnagels@users.noreply.github.com>
2018-09-24 11:00:58 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -9,3 +9,8 @@ wget https://www.syntevo.com/downloads/smartgit/smartgit-18_1_1.deb
 sudo gdebi smartgit-18_1_1.deb
 ##PyCharm
 sudo snap install pycharm-professional --classic
+
+sudo mkdir /opt/odoo
+if [ ! -d /opt/odoo/dynapps ]; then
+  git clone git@gitlab.dynapps.be:buildout/dynapps.git /opt/odoo/dynapps
+fi
