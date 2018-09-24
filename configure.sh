523ae3e17e3cbe634104475b5a96bd0d46ec7006
jimhoefnagels <jimhoefnagels@users.noreply.github.com>
2018-09-24 10:21:10 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,5 +3,9 @@ git pull
 #sudo add-apt-repository ppa:bionic-beaver/pycharm
 #sudo apt-get update
 #sudo apt-get install pycharm
-
+##SmartGit
+apt-get install gdebi -y
+wget https://www.syntevo.com/downloads/smartgit/smartgit-18_1_1.deb
+gdebi smartgit-18_1_1.deb
+##PyCharm
 sudo snap install pycharm-community --classic
