2ba6c337cd1475dd40cebd5693c377f6b2b6f5f5
jimhoefnagels <jimhoefnagels@users.noreply.github.com>
2018-09-24 10:06:29 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:bionic-beaver/pycharm
-sudo apt-get update
+#sudo add-apt-repository ppa:bionic-beaver/pycharm
+#sudo apt-get update
+#sudo apt-get install pycharm
 
-sudo apt-get install pycharm
+sudo snap install pycharm-community
