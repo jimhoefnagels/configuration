fa786a2421dfe784b5ecb6957832eb893473474f
Jim Hoefnagels <jim.hoefnagels@dynapps.be>
2018-12-04 14:20:42 +0100

    change wget to downloads

--- configure.sh
+++ configure.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
+DL='~/Downloads/'
 git pull
 #sudo add-apt-repository ppa:bionic-beaver/pycharm
 #sudo apt-get update
 #sudo apt-get install pycharm
 ##SmartGit
 sudo apt-get install -y gdebi python-virtualenv python-pip libxslt1-dev libxml2-dev
-if [ ! -f smartgit-18_1_1.deb ]; then
-   wget https://www.syntevo.com/downloads/smartgit/smartgit-18_1_1.deb
+if [ ! -f ~/Downloads/smartgit-18_1_1.deb ]; then
+   wget -P ~/Downloads/ https://www.syntevo.com/downloads/smartgit/smartgit-18_1_1.deb
 fi
-sudo gdebi smartgit-18_1_1.deb
-j
+sudo gdebi ~/Downloads/smartgit-18_1_1.deb
+
 ##PyCharm
 sudo snap install pycharm-professional --classic
 ##ssh config
 chmod 600 ~/.ssh/id.rsa
 ssh-add ~/.ssh/id.rsa
-git clone git@gitlab.dynapps.be:tools/ssh-config.git /opt/ssh-config
-ln -s ~/.ssh/config /opt/ssh-config/config
+git clone git@gitlab.dynapps.be:tools/ssh-config.git ~/ssh-config
+ln -fs ~/.ssh/config ~/ssh-config
 
-sudo mkdir /opt/odoo
-sudo chown $USER.$USER /opt/odoo
-mkdir /opt/odoo/buildouts
+if [ ! -d /opt/odoo ]; then
+    sudo mkdir /opt/odoo
+    sudo chown $USER.$USER /opt/odoo
+    mkdir /opt/odoo/buildouts
+fi
+
 for cust in dynapps; do
   if [ ! -d /opt/odoo/buildouts/${cust} ]; then
     git clone -b local git@gitlab.dynapps.be:buildout/${cust}.git /opt/odoo/buildouts/${cust}/local
   fi
   cd /opt/odoo/buildouts/${cust}/local/local
   virtualenv /opt/odoo/buildouts/${cust}/virtualenv --no-setuptools
-  /opt/odoo/buildouts/${cust}/virtualenv/bin/activate
+  . /opt/odoo/buildouts/${cust}/virtualenv/bin/activate
   python bootstrap.py
   deactivate
   # bin/buildout
 done
 
 # Install lastpass in firefox
-wget https://addons.mozilla.org/firefox/downloads/file/1133119/lastpass_password_manager-4.19.0.5-fx.xpi
-firefox lastpass_password_manager-4.19.0.5-fx.xpi
-
+if [ ! -f ~/Downloads/lastpass_password_manager-4.19.0.5-fx.xpi ]; then
+    cd ~
+    ll
+    wget -P ~/Downloads/ https://addons.mozilla.org/firefox/downloads/file/1133119/lastpass_password_manager-4.19.0.5-fx.xpi
+    firefox ~/Downloads/lastpass_password_manager-4.19.0.5-fx.xpi
+fi
