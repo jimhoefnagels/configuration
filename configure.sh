a5369e2cf9be77424e8a17dfa9b08299098ae6fc
jimhoefnagels <jimhoefnagels@users.noreply.github.com>
2018-12-18 08:49:08 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DL='~/Downloads/'
 OLD_BUILDOUTS='dynapps'
-NEW_BUILDOUTS='demo11 intracto'
+NEW_BUILDOUTS='demo12 demo11 intracto'
 BUILDOUT_ENVS='local testing'
 git pull
 #sudo add-apt-repository ppa:bionic-beaver/pycharm
