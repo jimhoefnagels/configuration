008b5f67b217e3462799abfad0ecc4e469ae3355
jimhoefnagels <jimhoefnagels@users.noreply.github.com>
2019-01-10 09:57:29 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 DL='~/Downloads/'
 OLD_BUILDOUTS=''
-NEW_BUILDOUTS='dynapps10 demo12 demo11 intracto twerk'
+NEW_P2BUILDOUTS='dynapps10'
+NEW_P3BUILDOUTS='dynapps10 demo12 demo11 intracto twerk'
 BUILDOUT_ENVS='local testing'
 SNAP='pycharm-professional htop setuptools slack'
 git pull
@@ -67,14 +68,27 @@ for cust in ${OLD_BUILDOUTS}; do
    deactivate
   # bin/buildout
 done
-for cust in ${NEW_BUILDOUTS}; do
+for cust in ${NEW_P2BUILDOUTS}; do
       echo 'cloning '${cust}
       if [ ! -d /opt/odoo/buildouts/${cust}/buildout ]; then
          git clone -b master git@gitlab.dynapps.be:customers/${cust}/buildout.git /opt/odoo/buildouts/${cust}/buildout
       fi
    cd /opt/odoo/buildouts/${cust}/buildout
    ln -s local.cfg buildout.cfg
-   virtualenv -p python3.5 /opt/odoo/buildouts/${cust}/virtualenv --no-setuptools
+   virtualenv /opt/odoo/buildouts/${cust}/virtualenv --no-setuptools
+   . /opt/odoo/buildouts/${cust}/virtualenv/bin/activate
+   python bootstrap.py
+   deactivate
+  # bin/buildout
+done
+for cust in ${NEW_P3BUILDOUTS}; do
+      echo 'cloning '${cust}
+      if [ ! -d /opt/odoo/buildouts/${cust}/buildout ]; then
+         git clone -b master git@gitlab.dynapps.be:customers/${cust}/buildout.git /opt/odoo/buildouts/${cust}/buildout
+      fi
+   cd /opt/odoo/buildouts/${cust}/buildout
+   ln -s local.cfg buildout.cfg
+   virtualenv -p python3.6 /opt/odoo/buildouts/${cust}/virtualenv --no-setuptools
    . /opt/odoo/buildouts/${cust}/virtualenv/bin/activate
    python bootstrap.py
    deactivate
